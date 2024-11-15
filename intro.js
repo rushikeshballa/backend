7dd19f7c13c9fc2c645ab6f56d9020f95dc5a2b4
rushikeshballa <rushikeshballa27@gmail.com>
2024-11-15 10:04:10 +0530

    asd

--- intro.js
+++ intro.js
@@ -1,3 +1,21 @@
-var fs= require('fs');
 
-
+const questionsData = Array.from({ length: 40 }, (_, index) => ({
+    section: Math.floor(index / 10) + 1, // Calculate section (1 to 4)
+    number: index + 1,
+    question: "Guess the output of the following program (Question ${index + 1})",
+    code: `
+          #include <stdio.h>
+          int main() {
+          int x = 10;
+          float y = 10.0;
+          if(x == y)
+          printf("x and y are equal");
+          else
+      printf("x and y are not equal");
+    return 0;
+  }
+  `,
+    options: ['x and y are not equal', 'x and y are equal', 'Run time error', 'Syntax error'],
+  }
+));
+console.log(questionsData);
