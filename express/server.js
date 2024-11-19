c8c994cc21395daa0dfce7f47a6df71265488d7f
rushikeshballa <rushikeshballa27@gmail.com>
2024-11-19 19:28:39 +0530

    asd

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 const express= require("express")
 const app=express()
+const students = require('./students')
 
 app.get("/",function(req,res) {
     res.send("home page")
@@ -10,6 +11,51 @@ app.get("/add/:fn/:fb",function (req,res) {
   res.send(x.toString())   
 })
 
+app.get("/index",function(req,res){
+    res.sendFile(__dirname+("/index.html"))
+})
+
+app.get("/students",function(req,res){
+        var tb="";
+        for(var i=0;i<=students.length-1;i++){
+            tb=tb+`
+            <tr>
+            <td>${students[i].firstname}</td>
+             <td>${students[i].lastname}</td>
+              <td>${students[i].class}</td>
+               <td><a href="student/${i}">view more </a></td>
+            </tr>   `
+            
+             
+        }
+        res.send(`<table border="1px" >${tb}</table>`)
+})
+
+
+app.get("/student/:i",function(req,res) {
+    var id=+req.params.i
+    var st=`
+    <h1>Fiestname:
+    ${students[id].firstname}
+    </h1>
+    <h1>Lastname:
+    ${students[id].lastname}
+    </h1>
+    <h1>Class:
+    ${students[id].class}
+    </h1>
+    <h1>Rollnumber:
+    ${students[id].rollnumber}
+    </h1>
+    <h1>Subjests:
+    ${[students[id].subjects]}
+    </h1>
+    <h1>Gender:
+    ${students[id].gender}
+    </h1>
+    `
+    res.send(st)
+})
 
 
 
---
+++ express/students.js
@@ -0,0 +1,323 @@
+module.exports=[
+    {
+      "firstname": "John",
+      "lastname": "Doe",
+      "rollnumber": 1,
+      "class": "10",
+      "subjects": ["Math", "Science", "English"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Jane",
+      "lastname": "Smith",
+      "rollnumber": 2,
+      "class": "10",
+      "subjects": ["Math", "History", "English"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Alice",
+      "lastname": "Johnson",
+      "rollnumber": 3,
+      "class": "10",
+      "subjects": ["Physics", "Chemistry", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Bob",
+      "lastname": "Brown",
+      "rollnumber": 4,
+      "class": "10",
+      "subjects": ["Geography", "Math", "English"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Charlie",
+      "lastname": "Williams",
+      "rollnumber": 5,
+      "class": "10",
+      "subjects": ["History", "Science", "Math"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Diana",
+      "lastname": "Miller",
+      "rollnumber": 6,
+      "class": "10",
+      "subjects": ["English", "Music", "History"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Ethan",
+      "lastname": "Davis",
+      "rollnumber": 7,
+      "class": "10",
+      "subjects": ["Math", "Computer Science", "Physics"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Fiona",
+      "lastname": "Martinez",
+      "rollnumber": 8,
+      "class": "10",
+      "subjects": ["Chemistry", "Biology", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "George",
+      "lastname": "García",
+      "rollnumber": 9,
+      "class": "10",
+      "subjects": ["Spanish", "Math", "Art"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Hannah",
+      "lastname": "Rodriguez",
+      "rollnumber": 10,
+      "class": "10",
+      "subjects": ["Literature", "History", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Ian",
+      "lastname": "Lopez",
+      "rollnumber": 11,
+      "class": "10",
+      "subjects": ["Math", "Biology", "English"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Julia",
+      "lastname": "Gonzalez",
+      "rollnumber": 12,
+      "class": "10",
+      "subjects": ["Physics", "Math", "Art"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Kevin",
+      "lastname": "Perez",
+      "rollnumber": 13,
+      "class": "10",
+      "subjects": ["Computer Science", "Math", "History"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Lily",
+      "lastname": "Wilson",
+      "rollnumber": 14,
+      "class": "10",
+      "subjects": ["Biology", "Math", "English"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Mason",
+      "lastname": "Anderson",
+      "rollnumber": 15,
+      "class": "10",
+      "subjects": ["Chemistry", "Math", "Geography"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Nina",
+      "lastname": "Thomas",
+      "rollnumber": 16,
+      "class": "10",
+      "subjects": ["Art", "Literature", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Oliver",
+      "lastname": "Taylor",
+      "rollnumber": 17,
+      "class": "10",
+      "subjects": ["Math", "Geography", "Music"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Penny",
+      "lastname": "Moore",
+      "rollnumber": 18,
+      "class": "10",
+      "subjects": ["History", "Chemistry", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Quinn",
+      "lastname": "Jackson",
+      "rollnumber": 19,
+      "class": "10",
+      "subjects": ["English", "History", "Biology"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Rachel",
+      "lastname": "White",
+      "rollnumber": 20,
+      "class": "10",
+      "subjects": ["Math", "Physics", "History"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Sam",
+      "lastname": "Harris",
+      "rollnumber": 21,
+      "class": "10",
+      "subjects": ["Math", "Literature", "Art"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Tina",
+      "lastname": "Martin",
+      "rollnumber": 22,
+      "class": "10",
+      "subjects": ["Music", "Geography", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Umar",
+      "lastname": "Clark",
+      "rollnumber": 23,
+      "class": "10",
+      "subjects": ["History", "Math", "Physics"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Vera",
+      "lastname": "Rodriguez",
+      "rollnumber": 24,
+      "class": "10",
+      "subjects": ["Science", "Math", "Music"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Will",
+      "lastname": "Lewis",
+      "rollnumber": 25,
+      "class": "10",
+      "subjects": ["Biology", "Chemistry", "Math"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Xena",
+      "lastname": "Walker",
+      "rollnumber": 26,
+      "class": "10",
+      "subjects": ["Literature", "Art", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Yara",
+      "lastname": "Hall",
+      "rollnumber": 27,
+      "class": "10",
+      "subjects": ["History", "Math", "Geography"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Zane",
+      "lastname": "Young",
+      "rollnumber": 28,
+      "class": "10",
+      "subjects": ["Math", "Physics", "Computer Science"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Amara",
+      "lastname": "King",
+      "rollnumber": 29,
+      "class": "10",
+      "subjects": ["English", "Math", "History"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Blake",
+      "lastname": "Scott",
+      "rollnumber": 30,
+      "class": "10",
+      "subjects": ["Chemistry", "Geography", "Math"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Caitlin",
+      "lastname": "Adams",
+      "rollnumber": 31,
+      "class": "10",
+      "subjects": ["Physics", "Math", "Literature"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "David",
+      "lastname": "Nelson",
+      "rollnumber": 32,
+      "class": "10",
+      "subjects": ["Math", "Computer Science", "Art"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Eva",
+      "lastname": "Carter",
+      "rollnumber": 33,
+      "class": "10",
+      "subjects": ["Science", "Math", "Music"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Felix",
+      "lastname": "Mitchell",
+      "rollnumber": 34,
+      "class": "10",
+      "subjects": ["History", "Math", "Biology"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Grace",
+      "lastname": "Perez",
+      "rollnumber": 35,
+      "class": "10",
+      "subjects": ["Literature", "Math", "Geography"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Henry",
+      "lastname": "Roberts",
+      "rollnumber": 36,
+      "class": "10",
+      "subjects": ["Math", "History", "Physics"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Isla",
+      "lastname": "Morris",
+      "rollnumber": 37,
+      "class": "10",
+      "subjects": ["Art", "Music", "Math"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Jack",
+      "lastname": "Miller",
+      "rollnumber": 38,
+      "class": "10",
+      "subjects": ["Biology", "Math", "Literature"],
+      "gender": "Male"
+    },
+    {
+      "firstname": "Kara",
+      "lastname": "Wilson",
+      "rollnumber": 39,
+      "class": "10",
+      "subjects": ["History", "Math", "Music"],
+      "gender": "Female"
+    },
+    {
+      "firstname": "Leo",
+      "lastname": "Hernandez",
+      "rollnumber": 40,
+      "class": "10",
+      "subjects": ["Math", "Science", "Art"],
+      "gender": "Male"
+    }
+  ]
+  
