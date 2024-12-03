25aba656576fec4309b91dde9342070133f86d94
rushikeshballa <rushikeshballa27@gmail.com>
2024-12-03 19:20:57 +0530

    asd

--- users/backend/server.js
+++ users/backend/server.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
+var mongoose=require('mongoose')
+var users = "mongodb+srv://rushikesh:@AsusR540ub@cluster0.jejlv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(users).then((res)=>{console.log(res)}).catch((err)=>{console.log(err)})
 
 
-app.get('/',((req,res)=>{
 
-}))
+
 
 
 app.listen(4500);
